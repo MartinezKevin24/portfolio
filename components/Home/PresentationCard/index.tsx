a600cd9594d00a4406ee7612fa23b28797cea6ff
MartinezKevin24 <kmartinez@liveventures.com>
2023-12-12 19:23:28 -0500

    Feat: working on personal data card

--- components/Home/PresentationCard/index.tsx
+++ components/Home/PresentationCard/index.tsx
@@ -6,7 +6,7 @@ export default function index() {
   return (
     <div className='bg-red-400 h-full flex justify-start px-4 py-6'>
       <Image src={memoji} alt={"memoji"} width={150} height={150} style={{objectFit: 'contain'}}/>
-      <div className='bg-blue-50 flex flex-col justify-end gap-2 py-6'>
+      <div className='flex flex-col justify-end gap-2 py-6'>
         <h1 className='font-bold text-2xl'>Aló. Welcome!</h1>
         <p>I am Kevin Martinez, a front-end developer with over 2 years of experience. I have a passion for upbeat music, dancing, and video games.</p>
       </div>
